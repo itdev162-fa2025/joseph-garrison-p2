e3d3e490f15be4ad19b4b180af458c67d2b66b88
Joe-414 <garrisj2@gmatc.matc.edu>
2024-11-29 23:06:49 +0100

    Added Sample toolbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
