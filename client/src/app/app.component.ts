d35926d3fb4f439ec08f4b4df961f11d7128cc6c
Joe-414 <garrisj2@gmatc.matc.edu>
2024-11-29 23:29:01 +0100

    Added Home component and routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -11,23 +10,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'COLOR HEX CODE BASE';
-  colors: any;
-
-  constructor(private http: HttpClient){
-
-  }
-
-  ngOnInit(): void {
-    this.http.get('http://localhost:5013/color').subscribe({
-      next: (response) => this.colors = response,
-      error: (e) => console.error(e),
-      complete: () => console.log('completed')      
-    });
-  }
-
-  showHexCode(colorObj:any): void {
-    alert(`Hexcode of ${colorObj.name}: ${colorObj.hexCode}`);
-  }
 }
